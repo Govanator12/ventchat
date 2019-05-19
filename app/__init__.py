787e55811092fec3abd42cbe522e099b6f916a61
Steven Hardy <steverhardy@gmail.com>
2019-05-19 14:04:12 -0400

    added jQuery. Rendering

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,22 @@
+import pusher
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config
 
 # setup app variables
 app = Flask(__name__)
+
+pusher_client = pusher.Pusher(
+  app_id='785700',
+  key='4f77cd0c59b6dd888f56',
+  secret='7a6333d7c3dbce5f117c',
+  cluster='us2',
+  ssl=True
+)
+
+
 bootstrap = Bootstrap(app)
 app.config.from_object(Config)
-
-
-# when a page requires somebody to logged in the application will by default
-# send them back to the previous page. However, we will make them go back to
-# the login instead
-login.login_view = 'login'
 
 # go to routes
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,16 @@
 from app import app
+from app.__init__ import  pusher_client
+from app.forms import ContactForm, PostForm
 from flask import render_template, url_for, redirect, request, jsonify
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    # pusher_client.trigger('my-channel', 'my-event', {'message': 'hello world'})
 
-    return render_template('index.html', title='Home')
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
